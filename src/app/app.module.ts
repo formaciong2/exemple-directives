578a731e712aea7fc1d2264944f608ff59dbe655
Cristian Requena <cristian.requena@t-systems.com>
2018-07-09 17:39:24 +0200

    Update to Angular6.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ColorpickerComponent } from './colorpicker/colorpicker.component';
-import { ColorComponent } from './colorpicker/color/color.component';
+import { ColorpickerComponent} from './colorpicker/colorpicker.component';
+import { ColorComponent} from './colorpicker/color/color.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ColorpickerComponent,
-    ColorComponent
+	ColorpickerComponent,
+	ColorComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+	FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
